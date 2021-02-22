4313e21e6b62ee7c9aa6504271e865d5107abbe0
clearwater <clearwater92@naver.com>
2021-02-22 14:06:50 +0900

    alphabet searching

---
+++ level1/203/10809.cpp
@@ -0,0 +1,25 @@
+// 10809 bronze 3
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+
+    for (int i = 'a'; i <= 'z'; i++) {
+        auto it = find(s.begin(), s.end(), i);
+        if (it == s.end()) {
+            cout << -1 << ' ';
+        }
+        else {
+            cout << (it - s.begin()) << ' ';
+        }
+    }
+
+    cout << '\n';
+
+
+    return 0;
+}
